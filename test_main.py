34969841764ea14dc2ba0d022baa428d3ec28004
Кирилл <qwinoxqwinox@gmail.com>
2023-03-28 11:52:21 +0300

    add_tests

--- test_main.py
+++ test_main.py
@@ -1,17 +1,15 @@
 from fastapi.testclient import TestClient
-
 import main
+
 client = TestClient(main.app)
 
 
-def test_home_route():
+def test_read_main():  # проверяет доступность приложения при обращении к корню сервер
     response = client.get("/")
     assert response.status_code == 200
 
-def test_predict_route():
-    file_name = 'static/images/dog.jpg'
-    
-    response = client.post(
-        "/predict",files={"file":("dog_image",open(file_name,"rb"),"image/jpeg")}
-        )
+
+def test_predict():  #
+    file_name = 'static/images/test_image.jpg'
+    response = client.post("/predict", files={"file": ("test_image", open(file_name, "rb"), "image/jpeg")})
     assert response.status_code == 200
